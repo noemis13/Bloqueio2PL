fea40b6ef950a47226ceb33a2d80cd37c55b904f
noemis13 <noemischerer13@gmail.com>
2016-10-22 11:45:05 -0200

    metade

--- jensen2-master/src/jensen2/Main.java
+++ jensen2-master/src/jensen2/Main.java
@@ -43,130 +43,184 @@ public class Main {
             Acesso operacao = dadosSchedule.getAcesso();
             String dado = dadosSchedule.getDado().getNome();
 
-            montaSchedule(idOperacao, indice, operacao, dado);
+            String schedule = null;
 
+            if (operacao.name().equals("S")) {
+                schedule = "S" + indice;
+            } else if (operacao.name().equals("E")) {
+                schedule = "E" + indice;
+            } else if (operacao.name().equals("R")) {
+                schedule = "R" + indice + "(" + dado + ")";
+            } else if (operacao.name().equals("W")) {
+                schedule = "W" + indice + "(" + dado + ")";
+            } else if (operacao.name().equals("C")) {
+                schedule = "C" + indice;
+            } else if (operacao.name().equals("A")) {
+                schedule = "A" + indice;
+            }
+
+            listaDeSchedule.add(schedule);
+            escalonarSchedule(i);
+            
             i++;
             //quando a lista tiver fazia ou nao puder continuar, deeve fazer uma nova consulta
         }
-
     }
 
     /*
-        Método responsável por receber uma schedule do banco
-        e montar em formato de schedule
-     */
-    public static void montaSchedule(int idOperacao, int indice, Acesso operacao, String dado) {
-        String s = null;
-        if (operacao.name().equals("S")) {
-            s = "S" + indice;
-        } else if (operacao.name().equals("E")) {
-            s = "E" + indice;
-        } else if (operacao.name().equals("R")) {
-            s = "R" + indice + "(" + dado + ")";
-        } else if (operacao.name().equals("W")) {
-            s = "W" + indice + "(" + dado + ")";
-        } else if (operacao.name().equals("C")) {
-            s = "C" + indice;
-        } else if (operacao.name().equals("A")) {
-            s = "A" + indice;
-        }
-
-        listaDeSchedule.add(s);
-        escalonarSchedule();
-    }
-
-    /*
-        
-        Método responsável por processar uma lista de schedule
+    Método responsável por processar uma lista de schedule
         e escalonar as operações
      */
-    public static void escalonarSchedule() {
-        
-        for (int i = 0; i < listaDeSchedule.size(); i++) {
+    public static void escalonarSchedule(int i) {
+        String transacao = listaDeSchedule.get(i);
+
+        if (transacao.contains("S")) {
+            transacaoEscalonada.add(transacao);
+
+        } else if (transacao.contains("E")) {
+            transacaoEscalonada.add(transacao);
+
+        } else if (transacao.contains("C")) {
+            transacaoEscalonada.add(transacao);
+
+        } else if (transacao.contains("W")) {
+            String[] splitOperacao = listaDeSchedule.get(i).split("\\(");
+            String apenasDado = splitOperacao[1];
+            splitOperacao = apenasDado.split("\\)");
+            apenasDado = splitOperacao[0];
+
+            String indiceOp = null;
+            String operacao = null;
+            String[] indice = listaDeSchedule.get(i).split("");
+            indiceOp = indice[1];
+            operacao = indice[0];
+
+            boolean escalonada;
+            boolean terminou;
+            escalonada = verificaDadosIguais(transacao, indiceOp, apenasDado, i);
+            realizaEscalonamento(escalonada, transacao);
             
-            if (listaDeSchedule.get(i).contains("S")) {
-                transacaoEscalonada.add(listaDeSchedule.get(i));
+        } else if (listaDeSchedule.get(i).contains("R")) {
+            String[] splitOperacao = listaDeSchedule.get(i).split("\\(");
+            String apenasDado = splitOperacao[1];
+            splitOperacao = apenasDado.split("\\)");
+            apenasDado = splitOperacao[0];
 
-            } else if (listaDeSchedule.get(i).contains("E")) {
-                transacaoEscalonada.add(listaDeSchedule.get(i));
+            String indiceOp = null;
+            String operacao = null;
+            String[] indice = listaDeSchedule.get(i).split("");
+            indiceOp = indice[1];
+            operacao = indice[0];
 
-            } else if (listaDeSchedule.get(i).contains("W")) {
-                String[] splitOperacao = listaDeSchedule.get(i).split("\\(");
-                String apenasDado = splitOperacao[1];
-                splitOperacao = apenasDado.split("\\)");
-                apenasDado = splitOperacao[0];
-
-
-                //verifica se alguem já está lendo
-                boolean igualdadeDado = verificaDadosIguais(listaDeSchedule.get(i),"W", apenasDado, i);
-                
-                if (igualdadeDado == true) {
-                    boolean opJaTerminou = encontraOp(listaDeSchedule.get(i), "W", apenasDado, i);
-                    System.out.println("PODE?? "+opJaTerminou);
-
-                } else {
-                    transacaoEscalonada.add(listaDeSchedule.get(i));
-                }
-
-            }
-
-        }//for
-
-    }
-
-    /*
-    Método responsável por verificar se um item do Schedule já existe
-    na lista de Schedule.
-    */
-    public static boolean verificaDadosIguais(String transacao, String operacao, String dado, int indiceTransacao){
-        boolean igualdadeDado = false;
-        
-        for (int i = 0; i < listaDeSchedule.size(); i++) {
-            String schedule = listaDeSchedule.get(i);
-            if(schedule.contains(operacao) && schedule.contains(dado) && i!=indiceTransacao){
-                igualdadeDado = true;
-            }
+            boolean escalonada;
+            boolean terminou;
+            escalonada = verificaDadosIguais(transacao, indiceOp, apenasDado, i);
+            realizaEscalonamento(escalonada, transacao);
         }
         
-        return igualdadeDado;
     }
-    
+
     /*
-        Método responsável por verificar se uma operação do schedule
-        pode ser escalonada ou se já está sendo executada por outra operacao.
-        Se pode, é adicionada na lista de operações escalonadas
-        Se não, é adicionada na lista de espera.
+    Método responsável por dado um dado da transação, 
+    verificar se existe um read ou write do mesmo dado em uma outra
+    transação diferente
      */
-    public static boolean encontraOp(String transacao, String operacao, String dado, int posValor) {
-        boolean operacaoExecutando = false;
-        boolean opJaTerminou = false;
-        
-        String indiceOp = null;        
-        for (int i = 0; i < listaDeSchedule.size(); i++) {
-            if (listaDeSchedule.get(i).contains(valor) && i != posValor) {
-                operacaoExecutando = true;
-                String[] splitIndice = listaDeSchedule.get(i).split("()");
-                indiceOp = splitIndice[1];
+    public static boolean verificaDadosIguais(String transacao, String indiceOpTransacao, String apenasDadoTransacao, int iTransacao) {
+        boolean escalona = false;
 
-            }
-        }
+        for (int i = 0; i < iTransacao; i++) {
+            //transacoes de mesmo dado com indices diferentes;
+            if (!listaDeSchedule.get(i).contains(indiceOpTransacao) && listaDeSchedule.get(i).contains(apenasDadoTransacao)) {
+                String[] split = listaDeSchedule.get(i).split("");
+                String operecao = split[0];
+                boolean verifica;
+                        
+                //verifica a operacao que possui o mesmo dado de indice diferente
+                if (operecao.equals("W")) {
+                    if (transacao.contains("R")) {
+                        //verifica se existe um read do mesmo indice e dado
+                        String readVerifica = "R" + indiceOpTransacao + "(" + apenasDadoTransacao + ")";
+                        verifica = verificaReadMesmoIndice(readVerifica, iTransacao);
+                        escalona = verificaTerminoTransacao(verifica, listaDeSchedule.get(i), iTransacao);
+                        
+                    } else {
+                        verifica = false;
+                        escalona = verificaTerminoTransacao(verifica, listaDeSchedule.get(i), iTransacao);
+                        
+                    }
 
-        //verifica se a operacao terminou
-        if (operacaoExecutando == true) {
-            String endOp = "E"+indiceOp;
-            for (String ls : listaDeSchedule) {
-                if (ls.equals(endOp)) {
-                    opJaTerminou = true;
-                    transacaoEscalonada.add(transacao);
+                } else if (operecao.equals("R")) {
+                    if (operecao.equals("W")) {
+                        verifica = false;
+                        escalona = verificaTerminoTransacao(verifica, listaDeSchedule.get(i), iTransacao);
+
+                    } else {
+                        escalona = true;
+                    }
                 }
-                else {
-                    listaEspera.add(transacao);
-                }
+            //a transacao pode ser escalonada    
+            } else {
+                escalona = true;
             }
-        }
-
-        return opJaTerminou;
-
+        }//for
+        return escalona;
     }
 
+    /*
+    Método responsável por verificar se existe um ReadX(dado) antes do WriteX(dado)
+    retorna se a transação pode ser escalonada ou nao
+     */
+    public static boolean verificaReadMesmoIndice(String t, int posOperacao) {
+        boolean escalonou = true;
+        for (int i = 0; i < posOperacao; i++) {
+            if (listaDeSchedule.get(i).equals(t)) {
+                escalonou = true;
+            } else {
+                escalonou = false;
+            }
+        }
+        return escalonou;
+    }
+
+    /*
+    Método responsável por verificar
+    se determinada transação já foi commitada ou terminada
+     */
+    public static boolean verificaTerminoTransacao(boolean escalonada, String transacao, int posOperacao) {
+        boolean terminou = false;
+        String[] indice = transacao.split("");
+        String valorIndice = indice[1];
+
+        String end = null;
+        end = "E" + valorIndice;
+
+        String commit = null;
+        commit = "C" + valorIndice;
+
+        if (escalonada == false) {
+            for (int i = 0; i < posOperacao; i++) {
+                if (listaDeSchedule.get(i).equals(end) || listaDeSchedule.get(i).equals(commit)) {
+                    terminou = true;
+                }
+
+            }
+        }
+
+        return terminou;
+    }
+
+
+    /*
+    Método resposável por adicionar transação na lista de escalonados
+    ou adicionar na lista de espera
+     */
+    public static void realizaEscalonamento(boolean escalonou, String transacao) {
+        if (escalonou == true) {
+            transacaoEscalonada.add(transacao);
+
+        } else {
+            listaEspera.add(transacao);
+        }
+        
+    }
 }
